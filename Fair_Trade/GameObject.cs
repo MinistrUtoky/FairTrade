1df8b757799ccc2b6ba4230efeb772822d2f1956
MinistrUtoky <dulov-vitya@mail.ru>
2022-10-12 21:40:47 +0300

    0

---
+++ Fair_Trade/GameObject.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fair_Trade
+{
+    internal class GameObject: Object
+    {
+
+    }
+}
